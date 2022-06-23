ce23afeea1ee7a1476e7447cedb0fefe3549ca29
cyace84 <cyace84@gmail.com>
2022-06-23 19:59:15 +0200

    added deploy funcs

--- migrations/01_vault.js
+++ migrations/01_vault.js
@@ -2,7 +2,9 @@ const { migrate } = require("../scripts/helpers");
 
 const vaultStorage = require("../storage/vault");
 const env = require("../env");
-
+const { Tezos } = require("../test/utils/cli");
+const vaultFunctions = require("../builds/lambdas/vault_lambdas.json");
+const { OpKind } = require("@taquito/taquito");
 module.exports = async tezos => {
   const sender = await tezos.signer.publicKeyHash();
   vaultStorage.storage.owner = sender;
@@ -27,5 +29,39 @@ module.exports = async tezos => {
 
   const contractAddress = await migrate(tezos, "vault", vaultStorage);
 
+  let batch1 = [];
+  let batch2 = [];
+
+  for (let i = 0; i < vaultFunctions.length; ++i) {
+    if (i < 16) {
+      batch1.push({
+        kind: OpKind.TRANSACTION,
+        to: contractAddress,
+        amount: 0,
+        parameter: {
+          entrypoint: "setup_func",
+          value: vaultFunctions[i],
+        },
+      });
+    } else {
+      batch2.push({
+        kind: OpKind.TRANSACTION,
+        to: contractAddress,
+        amount: 0,
+        parameter: {
+          entrypoint: "setup_func",
+          value: vaultFunctions[i],
+        },
+      });
+    }
+  }
+
+  let batch = Tezos.wallet.batch(batch1);
+  let operation = await batch.send();
+
+  await confirmOperation(Tezos, operation.opHash);
+  batch = Tezos.wallet.batch(batch2);
+  operation = await batch.send();
+  await confirmOperation(Tezos, operation.opHash);
   console.log(`Vault: ${contractAddress}`);
 };
