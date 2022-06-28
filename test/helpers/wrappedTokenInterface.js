b5f9277d48b6094237f3a5bcf9bbf82f3f3c7465
cyace84 <cyace84@gmail.com>
2022-06-29 01:29:49 +0200

    fixed tests

--- test/helpers/wrappedTokenInterface.js
+++ test/helpers/wrappedTokenInterface.js
@@ -7,10 +7,7 @@ module.exports = class WrappedToken extends Token {
     await this.updateStorage();
 
     try {
-      const balance = await this.storage.ledger.get({
-        spender: address,
-        value: tokenId,
-      });
+      const balance = await this.storage.ledger.get([address, tokenId]);
 
       return balance.toNumber();
     } catch (e) {
